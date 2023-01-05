475315498153f12e4e8305a4e42c7c2cd9b91e5d
tj <tharunkumar200230@gmail.com>
2023-01-05 12:37:33 +0530

    linked list done and dusted

--- codebasic/lists.py
+++ codebasic/lists.py
@@ -71,12 +71,3 @@ for i in range(1, max):
 print(odd_num)
 
 
-# max = int(input("Enter max number: "))
-
-# odd_numbers = []
-
-# for i in range(1, max):
-#     if i % 2 == 1:
-#         odd_numbers.append(i)
-
-# print("Odd numbers: ", odd_numbers)
